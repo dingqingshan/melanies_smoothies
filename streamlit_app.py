3d4e3b064115af7ec8c5ac4df8d52ea8d9e2c5c8
dingqingshan <52412435+dingqingshan@users.noreply.github.com>
2024-07-10 23:13:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,10 @@ if ingredients_list:
     ingredients_string='';
     for x in ingredients_list:
         ingredients_string += x +' ';
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(x + ' Nutrition Information ');
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + x)
         # st.text(fruityvice_response.json())
